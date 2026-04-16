a7adb6dea6c784582b811748a78d85bff1062b4b
Tyson Thomas <tysonthomas9@users.noreply.github.com>
2026-04-15 17:53:33 -0700

    Harden beads git test packages against GIT env leaks

---
+++ third_party/beads/internal/syncbranch/testmain_test.go
@@ -0,0 +1,23 @@
+package syncbranch
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// Strip GIT_* env vars that can redirect git subprocesses to the outer repo
+	// even when tests create and chdir into temp repositories.
+	for _, k := range []string{
+		"GIT_DIR",
+		"GIT_WORK_TREE",
+		"GIT_INDEX_FILE",
+		"GIT_OBJECT_DIRECTORY",
+		"GIT_ALTERNATE_OBJECT_DIRECTORIES",
+		"GIT_CEILING_DIRECTORIES",
+		"GIT_COMMON_DIR",
+	} {
+		_ = os.Unsetenv(k)
+	}
+	os.Exit(m.Run())
+}
