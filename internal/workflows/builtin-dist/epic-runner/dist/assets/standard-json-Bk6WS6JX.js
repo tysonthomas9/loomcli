63aae10803e1c0905f8ba547d21aab7a40b89823
Tyson Thomas <tysonthomas9@users.noreply.github.com>
2026-06-27 23:50:40 -0700

    Remove generated built-in workflow bundles

--- internal/workflows/builtin-dist/epic-runner/dist/assets/standard-json-Bk6WS6JX.js
+++
@@ -1,8 +0,0 @@
-import { b as __esmMin } from "../server.mjs";
-//#endregion
-__esmMin((() => {
-	throw new Error(`Could not resolve "effect" imported by "@standard-community/standard-json". Is it installed?`);
-}))();
-export {};
-
-//# sourceMappingURL=standard-json-Bk6WS6JX.js.map
--- internal/workflows/builtin-dist/github-review-agent/dist/assets/standard-json-Bls8W_uW.js
+++
@@ -1,6 +0,0 @@
-import { f as __esmMin } from "../server.mjs";
-//#endregion
-__esmMin((() => {
-	throw new Error(`Could not resolve "sury" imported by "@standard-community/standard-json". Is it installed?`);
-}))();
-export {};
