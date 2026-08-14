9fb8b53a679e650107e2bc5f916063de98a180a1
Tyson Thomas <tysonthomas9@users.noreply.github.com>
2026-08-14 10:31:24 -0700

    Add loom template CLI commands (list, show, apply)

--- cmd/loom/main.go
+++ cmd/loom/main.go
@@ -32,6 +32,7 @@ import (
 	_ "github.com/tysonthomas9/loomcli/internal/cli/serve/logroutercmd"
 	_ "github.com/tysonthomas9/loomcli/internal/cli/serve/worker"
 	_ "github.com/tysonthomas9/loomcli/internal/cli/stack"
+	_ "github.com/tysonthomas9/loomcli/internal/cli/teamtemplatecmd"
 	_ "github.com/tysonthomas9/loomcli/internal/cli/trigger"
 	_ "github.com/tysonthomas9/loomcli/internal/cli/workflow"
 	_ "github.com/tysonthomas9/loomcli/internal/cli/workspace"
